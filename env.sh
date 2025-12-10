0a1344612e95d56a43a54ecea96951e0e967ac2f
Adrien Blanchet <adrien.blanchet@live.fr>
2025-12-10 13:52:32 +0100

    fixing paths

--- env.sh
+++ env.sh
@@ -2,6 +2,7 @@
 
 export OA_INPUT_FOLDER=/sps/t2k/ablanche/work/results/gundam/gudi-anoa-sen25a/data
 
-export LD_LIBRARY_PATH=/sps/t2k/ablanche/work/results/gundam/gudi-anoa-sen25a/gundamOscAnaTools/resources/TabulateNuOscillator/build-x86_64/_deps/oscprob-src/lib:$LD_LIBRARY_PATH
+export NUOSCILLATOR_ROOT=/sps/t2k/ablanche/work/results/gundam/gudi-anoa-sen25a/gundamOscAnaTools/resources/TabulateNuOscillator/build-x86_64
 
-export LD_LIBRARY_PATH=/sps/t2k/ablanche/work/results/gundam/gudi-anoa-sen25a/gundamOscAnaTools/resources/TabulateNuOscillator/build-x86_64/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NUOSCILLATOR_ROOT/_deps/oscprob-src/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$NUOSCILLATOR_ROOT/lib:$LD_LIBRARY_PATH
