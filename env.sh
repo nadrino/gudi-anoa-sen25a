282faff31de9b8053f4592cdee3959920c107bab
Adrien Blanchet <adrien.blanchet@live.fr>
2025-12-10 13:44:42 +0100

    add env file

---
+++ env.sh
@@ -0,0 +1,7 @@
+
+
+export OA_INPUT_FOLDER=/sps/t2k/ablanche/work/results/gundam/gudi-anoa-sen25a/data
+
+export LD_LIBRARY_PATH=/sps/t2k/ablanche/work/results/gundam/gudi-anoa-sen25a/gundamOscAnaTools/resources/TabulateNuOscillator/build-x86_64/_deps/oscprob-src/lib:$LD_LIBRARY_PATH
+
+export LD_LIBRARY_PATH=/sps/t2k/ablanche/work/results/gundam/gudi-anoa-sen25a/gundamOscAnaTools/resources/TabulateNuOscillator/build-x86_64/lib:$LD_LIBRARY_PATH
